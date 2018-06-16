898971dd45cedb6d12520bb563c47b925f9699c2
mistGit <34454110+mistGit@users.noreply.github.com>
2018-06-16 09:58:57 +0200

    fixed the port issue

--- server/server.js
+++ server/server.js
@@ -5,12 +5,12 @@ const publicPath = path.join(__dirname + "/../public");
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(publicPath));
+app.use('/static',express.static(publicPath));
 
 app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Server is up on port ${port}`)
 });
