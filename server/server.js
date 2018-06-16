0653d470108ce29766ed2f760b93e1caa06ada89
mistGit <34454110+mistGit@users.noreply.github.com>
2018-06-16 10:53:04 +0200

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname + "/../public");
 const port = process.env.PORT || 3000;
-const app = express();
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
@@ -11,6 +15,14 @@ app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("device connected");
+
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`)
 });
